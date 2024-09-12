60c7f361589ce15966f2fac849e9c89e81e4ca2d
Kenneth Pham <kenneth.pham@gmail.com>
2024-09-12 07:19:27 -0700

    Used args_schema to enhance the tools defined meaningful arguments instead of __arg1, etc.

--- agents/tools/sql.py
+++ agents/tools/sql.py
@@ -2,6 +2,11 @@ import sqlite3
 
 from langchain.tools import Tool
 
+ # LangChain is stuck on pydantic v1, so extending a pydantic v2 base model won't work, use v1 specifically
+from pydantic.v1 import BaseModel
+
+from typing import List
+
 conn = sqlite3.connect('db.sqlite')
 
 def list_tables():
@@ -31,14 +36,22 @@ def run_sqlite_query(query):
   except sqlite3.OperationalError as err:
     return f'The following error occured: {str(err)}'
 
+class DescribeTablesArgsSchema(BaseModel):
+  table_name: List[str]
+
 describe_table_tool = Tool.from_function(
   name='describe_tables',  
   description='Given a list of table names, returns the schema of those tables',  # a must for ChatGPT to know what to do
-  func=describe_tables  # the method exact name above
+  func=describe_tables,  # the method exact name above
+  args_schema=DescribeTablesArgsSchema  # force the tool to name the arg as 'table_names'
 )
+
+class RunQueryArgsSchema(BaseModel):
+  query: str
 
 run_sqlite_tool = Tool.from_function(
   name='run_sqlite_query',
   description='Run a sqlite query',  # must be precise in order to tell ChatGPT what needs to be done
-  func=run_sqlite_query  # the method exact name above
+  func=run_sqlite_query,  # the method exact name above
+  args_schema=RunQueryArgsSchema  # force the tool to name the arg as 'query'
 )
