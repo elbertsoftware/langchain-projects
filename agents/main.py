da8157241281e706bedcf12158d48924ddc319d9
Kenneth Pham <kenneth.pham@gmail.com>
2024-09-12 05:50:50 -0700

    Added SystemMessage to describe table names"

--- agents/main.py
+++ agents/main.py
@@ -1,17 +1,23 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.schema import SystemMessage
 
 from dotenv import load_dotenv
 
-from tools.sql import run_sqlite_tool
+from tools.sql import list_tables, run_sqlite_tool
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+tables = list_tables()
+
 prompt = ChatPromptTemplate(
   messages=[
+    # only declare table names here in the SystemMessage to reduce message length
+    # detailed table schema will be provided via a new tool for ChatGPT to use when it needs
+    SystemMessage(content=f'You are an AI that has access to a SQLite database which has the following tables:\n{tables}'),
     HumanMessagePromptTemplate.from_template('{input}'),
     MessagesPlaceholder(variable_name='agent_scratchpad')  # agent_scratchpad acts like a simplified memory to keep track of the chat history
   ]
