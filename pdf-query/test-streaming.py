c0ee4b2771078d924e3c0e0e91f26008d364e06c
Kenneth Pham <kenneth.pham@gmail.com>
2024-10-06 14:52:03 -0700

    Extended LLMChain to override stream()

--- pdf-query/test-streaming.py
+++ pdf-query/test-streaming.py
@@ -11,7 +11,8 @@ from langchain_core.outputs import ChatGenerationChunk, GenerationChunk
 
 class StreamingHandler(BaseCallbackHandler):
   def on_llm_new_token(self, token, **kwargs):
-    print(token)
+    # print(token)
+    pass
 
 
 load_dotenv()
@@ -25,36 +26,58 @@ prompt = ChatPromptTemplate.from_messages([
     ('human', '{content}')
 ])
 
-chain = LLMChain(
-  llm=chat,
-  prompt=prompt
-)
+# chain = LLMChain(
+#   llm=chat,
+#   prompt=prompt
+# )
 
 # messages = prompt.format_messages(content="tell me a joke")
 # print(messages)
 
-# 1. no streaming with direct language mode
+# 1. no streaming from a LLM
 # output = chat(messages)
 # print(output)
 
-# 2. streaming with direct language model from langchain to the app
+# 2. streaming directly from a LLM (langchain to the app)
 # output = chat.stream(messages)  # stream() forces streaming capability no matter what streaming flag in ChatOpenAI instant is True/False 
 # print(output)
 
 # for o in output:
 #   print(o.content)
 
-# 3. no stream with chain
+# 3. no stream from a chain
 # output = chain('tell me a joke')
 # print(output)
 
-# 4. non implemented default streaming with chain
-output = chain.stream(
-  input={
-    'content': 'tell me a joke'
-  }
+# 4. non implemented default streaming from a chain
+# output = chain.stream(
+#   input={
+#     'content': 'tell me a joke'
+#   }
+# )
+
+# # print(output)
+# for o in output:
+#   print(o)
+
+# 5. Streaming from a chain
+
+class StreamingChain(LLMChain):
+  def stream(self, input):
+    self(input)  # make sure the stream method should run the chain
+
+    # print('hi there') 
+
+    # returning a generator that produces strings
+    yield 'hi'
+    yield ' there'
+
+
+chain = StreamingChain(
+  llm=chat,
+  prompt=prompt
 )
 
-# print(output)
-for o in output:
-  print(o)
+# print(chain('tell me a joke'))  # no streaming part is still working
+for output in chain.stream(input={'content': 'tell me a joke'}):
+  print(output)
