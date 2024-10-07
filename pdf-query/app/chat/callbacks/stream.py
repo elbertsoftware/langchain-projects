4d946e4a331e2a1cc4afbb48c996466c7c5bba07
Kenneth Pham <kenneth.pham@gmail.com>
2024-10-07 16:52:20 -0700

    Used different LLM for each chain: combine docs chain and condense question chain to avoid follow-up question become the answer

--- pdf-query/app/chat/callbacks/stream.py
+++ pdf-query/app/chat/callbacks/stream.py
@@ -4,14 +4,26 @@ from langchain.callbacks.base import BaseCallbackHandler
 class StreamingHandler(BaseCallbackHandler):
   def __init__(self, queue):
     self.queue = queue
+    self.streaming_run_ids = set()
+
+  def on_chat_model_start(self, serialized, messages, run_id, **kwargs):
+    # print(serialized)
+    # print(run_id)
+    if serialized['kwagrs']['streaming']:
+      # print('Streaming model: should listen to events with a run_id of', run_id)
+      self.streaming_run_ids.add(run_id)
 
   def on_llm_new_token(self, token, **kwargs):
     # streaming tokens arrive one by one
     # print(token)
     self.queue.put(token)
 
-  def on_llm_end(self, response, **kwargs):
-    self.queue.put(None)  # ending the while loop below since there is no more streaming
+  def on_llm_end(self, response, run_id, **kwargs):
+    if run_id in self.streaming_run_ids:
+      self.queue.put(None)  # ending the while loop below since there is no more streaming
+      self.streaming_run_ids.remove(run_id)
 
-  def on_llm_error(self, error, **kwargs):
-    self.queue.put(None)  # ending the while loop below in case of error
+  def on_llm_error(self, error, run_id, **kwargs):
+    if run_id in self.streaming_run_ids:
+      self.queue.put(None)  # ending the while loop below in case of error
+      self.streaming_run_ids.remove(run_id)
--- pdf-query/app/chat/chat.py
+++ pdf-query/app/chat/chat.py
@@ -1,3 +1,5 @@
+from langchain.chat_models import ChatOpenAI
+
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
@@ -20,5 +22,9 @@ def build_chat(chat_args: ChatArgs):
     return StreamingConversationalRetrievalChain.from_llm(
         llm=build_llm(chat_args),
         memory=build_memory(chat_args),
-        retriever=build_retriever(chat_args)
+        retriever=build_retriever(chat_args),
+
+        # specify a separate LLM for the internal condense question chain to use
+        # instead of sharing the same chain with combine doc chain
+        condense_question_llm=ChatOpenAI(streaming=False) 
     )
