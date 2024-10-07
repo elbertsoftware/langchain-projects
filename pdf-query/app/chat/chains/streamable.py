1398933fd4ac93dbdf69b57de90a46dc6098f46a
Kenneth Pham <kenneth.pham@gmail.com>
2024-10-07 10:02:53 -0700

    Fixed issue of working outside of application context defined by flask webapp

--- pdf-query/app/chat/chains/streamable.py
+++ pdf-query/app/chat/chains/streamable.py
@@ -2,6 +2,8 @@
 from queue import Queue
 from threading import Thread
 
+from flask import current_app
+
 from app.chat.callbacks.stream import StreamingHandler
 
 
@@ -10,13 +12,18 @@ class StreamableChain:
     queue = Queue()
     handler = StreamingHandler(queue)
 
-    def task():
+    def task(app_context):
+      app_context.push()  # add the thread into the existing flask webapp's content
+      
       self(
         input,
         callbacks=[handler]
       )
 
-    Thread(target=task).start()  # run this on a seperate thread
+    Thread(
+      target=task, 
+      args-[current_app.app_context()]  # make sure the thread will be part of the flask webapp's context
+    ).start()  # run this on a seperate thread
 
     while True:
       token = queue.get()
