8a97676851a0a07d7bf8ed12cebcea9d3cf7100e
Kenneth Pham <kenneth.pham@gmail.com>
2024-10-13 11:54:15 -0700

    Created a map of multiple LLM

--- pdf-query/app/chat/llms/chatopenai.py
+++ pdf-query/app/chat/llms/chatopenai.py
@@ -1,5 +1,8 @@
 from langchain.chat_models import ChatOpenAI
 
 
-def build_llm(char_args):
-  return ChatOpenAI(streaming=char_args.streaming)
+def build_llm(char_args, model_name):
+  return ChatOpenAI(
+    streaming=char_args.streaming,
+    model_name=model_name  # 'gpt-3.5-turbo' is used default
+  )
