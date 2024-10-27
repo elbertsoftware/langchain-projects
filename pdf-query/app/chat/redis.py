e34daf039fa194d4abcfb2f7c9e173a71b252f73
Kenneth Pham <kenneth.pham@gmail.com>
2024-10-27 06:24:23 -0700

    Stored votes in Redis hashes

--- pdf-query/app/chat/redis.py
+++ pdf-query/app/chat/redis.py
@@ -3,6 +3,6 @@ import redis
 
 
 client = redis.Redis.from_url(
-  os.environ('REDIS_URI'),
-  decode_response=True  # byte based data from Redis will be converted to Python strings
+  os.environ['REDIS_URI'],
+  decode_responses=True  # byte based data from Redis will be converted to Python strings
 )
--- pdf-query/app/chat/score.py
+++ pdf-query/app/chat/score.py
@@ -18,7 +18,17 @@ def score_conversation(conversation_id: str, score: float, llm: str, retriever: 
     score_conversation('abc123', 0.75, 'llm_info', 'retriever_info', 'memory_info')
     """
 
-    pass
+    score = min(max(score, 0), 1)  # make sure score falls b/w 0 and 1
+
+    # hash increase by
+    client.hincrby('llm_score_values', llm, score)  # increase value of key 'llm' in the hash 'llm_score_values' by score
+    client.hincrby('llm_score_counts', llm, 1)
+
+    client.hincrby('retriever_score_values', retriever, score)
+    client.hincrby('retriever_score_counts', retriever, 1)
+
+    client.hincrby('memory_score_values', memory, score)
+    client.hincrby('memory_score_counts', memory, 1)
 
 
 def get_scores():
